72ab49e34ffe757715b3a74d15333496100b7f65
MsakiHoshino <right1121@hotmail.co.jp>
2018-11-04 23:40:59 +0900

    TicketTypeのmigrationファイルの作成

--- db/migrate/20181104143746_create_ticket_types.rb
+++ db/migrate/20181104143746_create_ticket_types.rb
@@ -1,6 +1,8 @@
 class CreateTicketTypes < ActiveRecord::Migration
   def change
     create_table :ticket_types do |t|
+      t.string    :category   ,null: false
+      t.integer   :price      ,null: false
 
       t.timestamps null: false
     end
