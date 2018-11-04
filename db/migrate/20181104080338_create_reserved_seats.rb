338a1c3f37d8688a47c6317d863bab66b63079e7
moshi <right1121@hotmail.co.jp>
2018-11-04 17:49:59 +0900

    ReservedSeatのmigrationファイルの作成

--- db/migrate/20181104080338_create_reserved_seats.rb
+++ db/migrate/20181104080338_create_reserved_seats.rb
@@ -1,8 +1,12 @@
 class CreateReservedSeats < ActiveRecord::Migration
   def change
     create_table :reserved_seats do |t|
-
+        t.string        :seat_number    ,null: false
+        t.references    :movie          ,null: false
+        t.references    :user           ,null: false
       t.timestamps null: false
     end
+    add_foreign_key :reserved_seats, :movies
+    add_foreign_key :reserved_seats, :users
   end
 end
