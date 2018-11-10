78df65aaf826b45f4ceee61740b54bb9cb83b3fa
moshi <right1121@hotmail.co.jp>
2018-11-10 14:27:36 +0900

    テスト用データベースの設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104143746) do
+ActiveRecord::Schema.define(version: 20181104154043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,13 +46,16 @@ ActiveRecord::Schema.define(version: 20181104143746) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "name",                                          null: false
+    t.string   "sex"
+    t.string   "age",                    limit: 3
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
