ee20988ee78972e86da1bcc36ac45c7133556897
MsakiHoshino <right1121@hotmail.co.jp>
2018-11-13 01:32:56 +0900

    rails g active_admin:install の実行とmigration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ require "csv"
 CSV.foreach('db/TicketTypeData.csv') do |info|
   TicketType.create(category: info[0], price: info[1])
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
