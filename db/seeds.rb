19963268e3aedce5a92e3f9359a57af5f21f98d1
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-13 23:32:16 +0900

    映画とユーザーのシードを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ require "csv"
 CSV.foreach('db/TicketTypeData.csv') do |info|
   TicketType.create(category: info[0], price: info[1])
 end
+
+Movie.create(title: 'バック・トゥ・ザ・フューチャー', start_date: '2018-10-19', end_date: '2018-12-19',
+             start_time_array: ["10:00", "15:00", "19:00"], screen_time: 3)
+
+User.create(email: 'a@a.a', password: '123456', name: 'ドク', sex: '男', age: '65')
