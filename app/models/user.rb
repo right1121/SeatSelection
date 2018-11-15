2fe617d7605ffbc4ef93d2c08d3e844e1b0a270b
MsakiHoshino <right1121@hotmail.co.jp>
2018-11-16 01:50:57 +0900

    関連付けバグの修正
    綴りミス

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reseived_movies,  through: :reserved_seats,   source: :movie
-  has_many :reseived_seats,   dependent: :destroy
+  has_many :reserved_seats,   dependent: :destroy
 
 end
