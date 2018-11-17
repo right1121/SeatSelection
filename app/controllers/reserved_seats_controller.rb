79da1f51451aeb9a9bcd360b1ef191f307e18107
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-17 20:02:43 +0900

    仮座席予約機能　予約している席の表示を変える

--- app/controllers/reserved_seats_controller.rb
+++ app/controllers/reserved_seats_controller.rb
@@ -4,7 +4,25 @@ class ReservedSeatsController < ApplicationController
   
   def new
     @reserved_seat = ReservedSeat.new
+    @buried_seats = ReservedSeat.where(movie_id: params[:movie_id])  #映画の予約済みの座席を呼び出す
   end
 
-  
+  def create
+    @user = User.find(1) #仮実装。current_userにする
+    @reserved_seat = @user.reserved_seats.new(reserved_seat_params)
+    @reserved_seat.movie_id = params[:movie_id]
+    if @reserved_seat.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def reserved_seat_params
+    params.require(:reserved_seat).permit(
+      :seat_number,
+      :movie_id,
+      :user_id
+    )
+  end
 end
