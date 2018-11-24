7bff783271d2e25631fcdbe90b2bc4d5e9707a45
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-24 21:16:57 +0900

    座席情報の値を変更

--- app/helpers/reserved_seats_helper.rb
+++ app/helpers/reserved_seats_helper.rb
@@ -2,7 +2,7 @@ module ReservedSeatsHelper
     def check_reserved_seat(buried_seats_array, i, j)
         check = false
         buried_seats_array.each do |buried_seat|
-            if buried_seat == "#{i} #{j}"
+            if buried_seat == "#{i}-#{j}"
                 check = true
             end
         end
