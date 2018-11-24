6ddba3bc023b2a4926ebfb99851ec3bd29992109
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-24 22:25:37 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-=======
   get 'reserved_seats/index'
 
->>>>>>> 22_reserveSeats
   devise_for :users, module: :users
   root 'pages#index'
 
