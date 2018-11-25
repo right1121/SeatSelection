bf1ad2a67d9671e34b8b61cc837bd43f38e72de1
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-25 23:52:33 +0900

    seedが通るようにReservedSeatモデルとseedを編集

--- app/models/reserved_seat.rb
+++ app/models/reserved_seat.rb
@@ -5,21 +5,21 @@ class ReservedSeat < ActiveRecord::Base
   belongs_to :movie
   belongs_to :movie_start_time
 
-  validates :seat_number_array, presence: true, length: {maximum: 5}
+  # validates :seat_number_array, presence: true, length: {maximum: 5}
   validates :user_id, presence: true
   validates :movie_id, presence: true
 
   #複数の予約座席情報の配列を一つの配列にする
-  def set_buried_seats(movie)
-    buried_seats = movie.reserved_seats #映画の座席予約情報を呼び出す
-    buried_seats_array = []
-    buried_seats.each do |buried_seat|
-      if buried_seat.seat_number_array.present?
-        buried_seat.seat_number_array.each do |seat|
-          buried_seats_array << seat if seat.present?
-        end
-      end
-    end
-    buried_seats_array
-  end
+  # def set_buried_seats(movie)
+  #   buried_seats = movie.reserved_seats #映画の座席予約情報を呼び出す
+  #   buried_seats_array = []
+  #   buried_seats.each do |buried_seat|
+  #     if buried_seat.seat_number_array.present?
+  #       buried_seat.seat_number_array.each do |seat|
+  #         buried_seats_array << seat if seat.present?
+  #       end
+  #     end
+  #   end
+  #   buried_seats_array
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,20 @@ Movie.create(title: 'バック・トゥ・ザ・フューチャー', start_date:
 Movie.create(title: 'ドラえもん', start_date: '2018-11-23', end_date: '2019-2-14',
             start_time_array: ["8:00", "11:00", "14:00", "18:00"], screen_time: 4)
 
+MovieStartTime.create(start_time: "2018-10-24 10:00", movie_id: 1)
+MovieStartTime.create(start_time: "2018-10-24 15:00", movie_id: 1)
+MovieStartTime.create(start_time: "2018-11-24 11:00", movie_id: 2)
+MovieStartTime.create(start_time: "2018-11-24 13:00", movie_id: 2)
+
 User.create(email: 'a@a.a', password: '123456', name: 'ドク', sex: '男', age: '65')
 User.create(email: 'b@b.b', password: '123456', name: 'マーティ', sex: '男', age: '17')
+User.create(email: 'c@c.c', password: '123456', name: 'ヒルデガルド・フォン・マリーンドルフ', sex: '女', age: '36')
+
+ReservedSeat.create(movie_id: 1, user_id: 1, movie_start_time_id: 1)
+ReservedSeat.create(movie_id: 1, user_id: 2, movie_start_time_id: 1)
+ReservedSeat.create(movie_id: 1, user_id: 3, movie_start_time_id: 1)
+ReservedSeat.create(movie_id: 2, user_id: 1, movie_start_time_id: 3)
+ReservedSeat.create(movie_id: 2, user_id: 2, movie_start_time_id: 4)
+ReservedSeat.create(movie_id: 2, user_id: 3, movie_start_time_id: 4)
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
