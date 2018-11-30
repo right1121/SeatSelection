1b3c80ab58a89591335b535c090dd46897e75139
kakeyatomo <danbain.tobimasu@gmail.com>
2018-11-30 21:04:50 +0900

    reserved_seatモデルにバリデーション追加

--- app/helpers/reserved_seats_helper.rb
+++ app/helpers/reserved_seats_helper.rb
@@ -1,13 +1,6 @@
 module ReservedSeatsHelper
     def check_reserved_seat_helper(movie_start_time, i, j)
         check = false
-        # array = []
-        # movie_start_time.reserved_seats.each do |reserved_seat|
-        #     array << reserved_seat.seat_
-        #     if buried_seat == "#{i}-#{j}"
-        #         check = true
-        #     end
-        # end
         reserved_seats = movie_start_time.reserved_seats
         if reserved_seats.find_by(seat_number_1: "#{i}-#{j}")\
             || reserved_seats.find_by(seat_number_2: "#{i}-#{j}")\
--- app/models/reserved_seat.rb
+++ app/models/reserved_seat.rb
@@ -7,6 +7,8 @@ class ReservedSeat < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :movie_id, presence: true
+  validates :movie_start_time, presence: true
+  validates :seat_number_1, presence: true
 
   #複数の予約座席情報の配列を一つの配列にする
   def set_seat_number(seat_number_array)
@@ -19,15 +21,5 @@ class ReservedSeat < ActiveRecord::Base
       when 5 then self.seat_number_5 = seat_number
       end
     end
-    # buried_seats = movie.reserved_seats #映画の座席予約情報を呼び出す
-    # buried_seats_array = []
-    # buried_seats.each do |buried_seat|
-    #   if buried_seat.seat_number_array.present?
-    #     buried_seat.seat_number_array.each do |seat|
-    #       buried_seats_array << seat if seat.present?
-    #     end
-    #   end
-    # end
-    # buried_seats_array
   end
 end
