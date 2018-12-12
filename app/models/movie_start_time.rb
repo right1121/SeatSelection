dd8b92dc60730056bdef0f9d5812e2b31a05a050
kakeyatomo <danbain.tobimasu@gmail.com>
2018-12-13 07:09:03 +0900

    曜日ごとに予約ボタンを表示したい（途中）

--- app/models/movie_start_time.rb
+++ app/models/movie_start_time.rb
@@ -2,4 +2,8 @@ class MovieStartTime < ActiveRecord::Base
   has_many :reserved_seats, dependent: :destroy
 
   belongs_to :movie
+
+  # def self.pick_date(date)
+  #   self.where(start_time: )
+  # end
 end
